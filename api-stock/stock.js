4b9c44e4fcc0d53513c573a2f43c77da8698ee5c
frankMarcelino <frank.silva.ti@gmail.com>
2024-08-17 19:06:10 -0300

    first commit da API

---
+++ api-stock/stock.js
@@ -0,0 +1,27 @@
+const stock = [
+  {
+    id: 1,
+    productName: "Product 1",
+    amountLeft: 100,
+  },
+  {
+    id: 2,
+    productName: "Product 2",
+    amountLeft: 150,
+  },
+  {
+    id: 3,
+    productName: "Product 3",
+    amountLeft: 200,
+  },
+  {
+    id: 4,
+    productName: "Product 4",
+    amountLeft: 100,
+  },
+  {
+    id: 5,
+    productName: "Product 5",
+    amountLeft: 50,
+  },
+];
