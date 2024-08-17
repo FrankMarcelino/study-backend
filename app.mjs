87e22bad20d6ea6e8d0eadb650025467a4a6fdb0
frankMarcelino <frank.silva.ti@gmail.com>
2024-08-17 17:21:24 -0300

    criado acesso ao index.html

--- app.mjs
+++ app.mjs
@@ -1,12 +1,24 @@
 import http from "node:http";
+import url from "node:url";
+import fs from "node:fs/promises";
+import { createReadStream } from "node:fs";
 
 const server = http.createServer();
 
-server.addListener("request", (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.write("meu primeiro servidor");
-  res.end();
+server.on("request", async (req, res) => {
+  const filePath = url.parse(req.url).pathname;
+  const processPath = process.cwd();
+
+  try {
+    await fs.stat(processPath + filePath);
+    const readStream = createReadStream(processPath + filePath);
+    res.writeHead(200, { "content-type": "text/html" });
+    readStream.pipe(res);
+  } catch (err) {
+    res.writeHead(404);
+    res.end("Not found");
+    return;
+  }
 });
 
 server.listen(8000);
